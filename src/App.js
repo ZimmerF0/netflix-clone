47ec914644b96b62f5d9684511d3ad87630034f9
Kishkin Vitalii <dimpla30@gmail.com>
2023-06-15 15:40:30 +0300

    building loginScreen and signupScreen components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
+import { auth } from "./firebase";
 
 function App() {
+  const user = null;
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // Logged in
+        console.log(userAuth);
+      } else {
+        // Logged out
+      }
+    });
+
+    return unsubscribe;
+  }, []);
+
   return (
     <div className="app">
       <Router>
-        <Routes>
-          <Route exact path="/" element={<HomeScreen />} />
-        </Routes>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />} />
+          </Routes>
+        )}
       </Router>
     </div>
   );
--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,20 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyAsXyRxYqlG09EMN4wVNlQj34_XZcXDSmo",
   authDomain: "netflix-clone-82804.firebaseapp.com",
   projectId: "netflix-clone-82804",
   storageBucket: "netflix-clone-82804.appspot.com",
   messagingSenderId: "350294532023",
-  appId: "1:350294532023:web:a46aeef2b2195bfdbbd232",
+  appId: "1:350294532023:web:a46aeef2b2195bfdbbd232"
 };
+
+const firebaseApp = initializeApp(firebaseConfig);
+const auth = getAuth(firebaseApp);
+const db = getFirestore();
+
+export { auth, db };
+export default firebaseApp;
